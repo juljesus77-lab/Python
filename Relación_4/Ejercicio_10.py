8ae0acd82f423f2e58454fda1163d1304ab5941e
julio <juljesus77@gmail.com>
2025-11-12 21:35:00 +0100

    Ejercicio_10(rel4)-sinacabar

---
+++ Relación_4/Ejercicio_10.py
@@ -0,0 +1,8 @@
+num_ent = int ( input("Introduzca un número entero: ") )
+
+if  (num_ent % num_ent == 0 and num_ent % 1 == 0 ):
+    print ("El número es primo")
+
+for i in range (num_ent, , +1 )
+
+

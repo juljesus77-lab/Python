1660702a85b68ce7e9ea074451dbc63d6a9ef706
JulioLopez <juljesus77@gmail.com>
2025-12-04 10:23:59 +0100

    Ej_3(rel5)

---
+++ Relación_5/Ej_3.py
@@ -0,0 +1,6 @@
+asignaturas = ["matemáticas","física","química","historia","lengua"]
+for i in asignaturas:
+    p= input ("¿Qué nota ha sacado en "+i+"?")
+    nota = p
+    for j in nota:
+        print ("En "+i+" ha sacado "+nota)

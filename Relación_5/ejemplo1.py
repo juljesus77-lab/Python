ea711d1692053a7d060ad2d573161e4019b0d554
JulioLopez <juljesus77@gmail.com>
2025-12-04 09:55:47 +0100

    Ejemplos listas

---
+++ Relación_5/ejemplo1.py
@@ -0,0 +1,21 @@
+lista = ["lunes","martes","miércoles"]
+lista2 = ["jueves","viernes"]
+#lista3 = lista + lista2
+#print(lista3)
+print(lista)
+lista.insert(1,"viernes")
+print(lista)
+
+#print(lista[0])
+#lista.append("jueves")  #se añade el elemento al final de la lista
+#print(lista)
+#lista.sort()    # se ordena por orden alfabético
+#lista.sort(reverse=True)
+#print(lista)
+#lista[0]="viernes"
+#lista.remove("lunes")
+#print(lista)
+#for i in lista:
+#   print(i)
+
+
---
+++ Relación_5/ejemplo2.py
@@ -0,0 +1,5 @@
+a = [[1,2],[3,4]]
+print(a)
+for i in range (len(a)):
+    for j in range(len(a[0])):
+        print(a[i][j])

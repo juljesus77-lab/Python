fabebdebf9831e46dde4d19814763b68e4b01569
JulioLopez <juljesus77@gmail.com>
2025-12-04 11:13:18 +0100

    cambios ej4(rel5)

--- Relación_5/Ej_4.py
+++ Relación_5/Ej_4.py
@@ -1,5 +1,19 @@
-n1 = input("Introduzca el primer número ganador ")
-n2 = input("Introduzca el segundo número ganador ")
-l = [n1,n2]
-l.sort(reverse = True)
-print(l)
+for i in range (1,5,1):
+    num = int(input("Introduzca los números ganadores "))
+    print 
+
+lista = []
+opcion = int(input ("Introduzca una opción: "))
+print ("1. Añadir número")
+print ("2. Salir")
+if (opcion == 1):
+    num = int(input("Introduzca el número a añadir"))
+    lista.append(num)
+elif (opcion == 2):
+    c = 0
+    
+
+
+
+
+

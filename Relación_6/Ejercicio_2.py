b6a6acd45f3c0d26e5e069765ee7c1a1f4c8f13e
JulioLopez <juljesus77@gmail.com>
2026-01-08 11:15:42 +0100

    Ejercicio_2(dicc-sin acabar)

---
+++ Relación_6/Ejercicio_2.py
@@ -0,0 +1,6 @@
+nombre = input ("Introduzca su nombre")
+edad = int(input("introduzca su edad"))
+direccion = input("ponga su direccion")
+telefono = int(input("ponga su tgelefono"))
+print (nombre + "tiene " + edad + " años ", "vive en " + direccion + " y su número de
+teléfono es " <teléfono>")

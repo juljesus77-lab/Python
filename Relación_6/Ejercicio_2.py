dacdd065659570d5e9baa1aa34f149715d3f2e35
JulioLopez <juljesus77@gmail.com>
2026-01-09 14:17:01 +0100

    Ejercicio_2(dicc)

--- Relación_6/Ejercicio_2.py
+++ Relación_6/Ejercicio_2.py
@@ -1,6 +1,14 @@
-nombre = input ("Introduzca su nombre")
-edad = int(input("introduzca su edad"))
-direccion = input("ponga su direccion")
-telefono = int(input("ponga su tgelefono"))
-print (nombre + "tiene " + edad + " años ", "vive en " + direccion + " y su número de
-teléfono es " <teléfono>")
+nombre = input ("Introduzca su nombre ")
+edad = int(input("introduzca su edad "))
+direccion = input("ponga su direccion ")
+telefono = int(input("ponga su telefono "))
+
+thisdict =	{
+  "nombre": nombre,
+  "edad": edad,
+  "direccion": direccion,
+  "telefono": telefono
+}
+
+
+print (thisdict["nombre"] + " tiene " + str(thisdict["edad"]) + " años ", "vive en " + thisdict["direccion"] + " y su número de teléfono es " + str(thisdict["telefono"]) )

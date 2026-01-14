2ffa1ef974c53980ef44013b570a5563fa07c437
julio <juljesus77@gmail.com>
2026-01-14 21:37:26 +0100

    cambios

--- repaso_examen/pruebas.py
+++
@@ -1,8 +0,0 @@
-num = 7
-for i in range (0,num):
-    total = i + 1
-    print ("*"* total)
-
-for j in range (0,num):
-    total = j - 1
-    print ("*"* total)
--- repaso_examen/rombo.py
+++
@@ -1,7 +0,0 @@
-num = int (input("Introduzca un número entero impar "))
-for i in range (0,num):
-    total = i + 1
-    print ("*"* total)
-
-
-

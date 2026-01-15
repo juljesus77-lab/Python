01128a8cd2afda4ca4e30877a963d9868ba0cc8a
JulioLopez <juljesus77@gmail.com>
2026-01-15 10:10:13 +0100

    Ejercicio_5(dicc)-sin_acabar

---
+++ Relación_6/Ejercicio_5.py
@@ -0,0 +1,8 @@
+creditos = {'Matemáticas': 6, 
+            'Física': 4, 
+            'Química': 5}
+
+for i in range ():
+
+suma_creditos = creditos["Matemáticas"] + creditos["Física"] + creditos["Química"]
+print ("Total de créditos: " + str(suma_creditos) )

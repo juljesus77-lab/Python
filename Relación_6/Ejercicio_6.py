7389325f0b94cb07a7bd6f7037f722a64e9a1640
JulioLopez <juljesus77@gmail.com>
2026-01-15 10:23:17 +0100

    Ejercicio_6(dicc)

---
+++ Relación_6/Ejercicio_6.py
@@ -0,0 +1,19 @@
+thisdict= {}
+
+nombre = input ("Introduzca su nombre ")
+thisdict["nombre"]=nombre
+print (thisdict)
+
+edad = int(input ("Introduzca su edad "))
+thisdict["edad"]=edad
+print (thisdict)
+
+sexo = input("Introduzca su género ")
+thisdict["sexo"]=sexo
+print (thisdict)
+
+telefono = input("Introduzca su correo electrónico ")
+thisdict["telefono"]=telefono
+print (thisdict)
+
+

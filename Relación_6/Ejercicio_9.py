1299f12c1e69b6e3fc994caf561b1c4b7a331f6e
JulioLopez <juljesus77@gmail.com>
2026-01-16 14:41:52 +0100

    Ej_9(dicc)-sin_acabar

---
+++ Relación_6/Ejercicio_9.py
@@ -0,0 +1,13 @@
+facturas = {}
+
+
+contador = 1
+while contador == 1:
+    p = input("Elija una de las siguientes opciones: ")
+    print ("1. Añadir factura")
+    print ("2. Pagar una existente")
+    print ("Terminar")
+
+
+
+"num_fac:coste"

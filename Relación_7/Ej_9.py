8b012edca627531285a2b1cc3c6ad9773270feda
JulioLopez <juljesus77@gmail.com>
2026-01-23 14:41:35 +0100

    Ej_9(funciones)-intento

---
+++ Relación_7/Ej_9.py
@@ -0,0 +1,12 @@
+def mcd(num1,num2):
+    numeros_primos=[2,3,5,7,11]
+    for i in numeros_primos:
+        if num1 % i == 0:   
+            div1= num1 / 1
+            div2= num2 / 2
+    
+print(mcd(10,5))
+
+# MCD: Es el número máximo en el que dos números pueden dividirse y el resto da 0
+    
+
